83d6549fef58a61560ef43157634b508b855828c
Biplob798 <mdbiplop798@gmail.com>
2023-11-27 01:26:13 +0600

    setup with mongoDB and get listing data

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ app.use(express.json())
 const { MongoClient, ServerApiVersion } = require('mongodb');
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.vsymadz.mongodb.net/?retryWrites=true&w=majority`;
 
-// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+
 const client = new MongoClient(uri, {
   serverApi: {
     version: ServerApiVersion.v1,
@@ -26,9 +26,29 @@ const client = new MongoClient(uri, {
 
 async function run() {
   try {
-    // Connect the client to the server	(optional starting in v4.7)
+    
     await client.connect();
-    // Send a ping to confirm a successful connection
+
+    // DB collection
+
+    const listingCollection = client.db('petDB').collection('listings')
+    const categoryCollection = client.db('petDB').collection('categories')
+
+
+
+    app.get('/listings', async(req,res)=>{
+        const result= await listingCollection.find().toArray()
+        res.send(result)
+    })
+    app.get('/categories', async(req,res)=>{
+        const result= await categoryCollection.find().toArray()
+        res.send(result)
+    })
+
+
+
+
+   
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
