06b07b5a6ba23b8a7090dd2cd28e7f89e7ecbc4e
Biplob798 <mdbiplop798@gmail.com>
2023-11-28 23:56:55 +0600

    create admin api and get user data

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 const express = require('express')
 const app = express()
 const cors = require('cors')
+const jwt = require('jsonwebtoken');
 require('dotenv').config()
-const port =process.env.PORT || 5000
+const port = process.env.PORT || 5000
 
 // middleware 
 
@@ -26,42 +27,118 @@ const client = new MongoClient(uri, {
 
 async function run() {
   try {
-    
+
     await client.connect();
 
-    // DB collection
 
+
+
+
+    // ------------------
+    // DB all collection
+
+    const userCollection = client.db('petDB').collection('users')
     const listingCollection = client.db('petDB').collection('listings')
+    const petCollection = client.db('petDB').collection('pets')
     const categoryCollection = client.db('petDB').collection('categories')
     const campingCollection = client.db('petDB').collection('campings')
 
 
-    // listing data 
-    app.get('/listings', async(req,res)=>{
-        const result= await listingCollection.find().toArray()
+
+    // user related api 
+
+    app.get('/users', async (req, res) => {
+
+      const result = await userCollection.find().toArray()
+      res.send(result)
+    })
+
+
+
+    app.post('/users', async (req, res) => {
+      const user = req.body
+      const query = { email: user.email }
+      const existingUser = await userCollection.findOne(query)
+      if (existingUser) {
+        return res.send({ message: 'user already exist', insertedId: null })
+      }
+      const result = await userCollection.insertOne(user)
+      res.send(result)
+    })
+
+      // Admin Api  
+      app.patch('/users/admin/:id', async (req, res) => {
+        const id = req.params.id
+        const filter = { _id: new ObjectId(id) }
+        const updatedInfo = {
+            $set: {
+                role: 'admin'
+            }
+        }
+        const result = await userCollection.updateOne(filter, updatedInfo)
         res.send(result)
     })
-    // category data 
-    app.get('/categories', async(req,res)=>{
-        const result= await categoryCollection.find().toArray()
+
+
+    app.delete('/users/:id', async (req, res) => {
+        const id = req.params.id
+        const query = { _id: new ObjectId(id) }
+        const result = await userCollection.deleteOne(query)
         res.send(result)
     })
-    // camping data 
-    app.get('/campings', async(req,res)=>{
-        const result= await campingCollection.find().toArray()
-        res.send(result)
+
+
+    // all data api -----
+
+    // listing data collection
+    app.get('/listings', async (req, res) => {
+      const result = await listingCollection.find().toArray()
+      res.send(result)
+    })
+    // category data collection
+    app.get('/categories', async (req, res) => {
+      const result = await categoryCollection.find().toArray()
+      res.send(result)
+    })
+    // camping data collection
+    app.get('/campings', async (req, res) => {
+      const result = await campingCollection.find().toArray()
+      res.send(result)
     })
     app.get('/campings/:id', async (req, res) => {
       const id = req.params.id
       const query = { _id: new ObjectId(id) }
       const result = await campingCollection.findOne(query)
       res.send(result)
-  })
+    })
+
+    // pets data collection
+
+    app.get('/pets', async (req, res) => {
+      const email = req.query.email
+      const query = { email: email }
+      const result = await petCollection.find(query).toArray()
+      res.send(result)
+    })
+
+    app.post('/pets', async (req, res) => {
+      const petItem = req.body
+      const result = await petCollection.insertOne(petItem);
+      res.send(result)
+    })
+
+    app.delete('/pets/:id', async (req, res) => {
+      const id = req.params.id
+      const query = { _id: new ObjectId(id) }
+      const result = await petCollection.deleteOne(query)
+      res.send(result)
+    })
 
 
 
 
-   
+
+
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
@@ -72,9 +149,9 @@ async function run() {
 run().catch(console.dir);
 
 
-app.get('/',(req,res)=>{
-    res.send('pet is coming')
+app.get('/', (req, res) => {
+  res.send('pet is coming')
 })
-app.listen(port,()=>{
-    console.log(`Pet is coming soon port${port}`)
+app.listen(port, () => {
+  console.log(`Pet is coming soon port${port}`)
 })
